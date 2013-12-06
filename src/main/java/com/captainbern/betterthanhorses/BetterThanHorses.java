2fb53e874cf20806a541ca39998fbdd125facd80
CaptainBern <captainberneby@gmail.com>
2013-12-06 18:40:54 +0100

    Like horses but better.

---
+++ src/main/java/com/captainbern/betterthanhorses/BetterThanHorses.java
@@ -0,0 +1,21 @@
+package com.captainbern.betterthanhorses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterThanHorses extends JavaPlugin {
+
+    /**
+     * Like horses, but better.
+     */
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
